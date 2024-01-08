429cd26deee84b866fb40208eddfd8d8138116d4
Alexandre78R <alexandre.renard98@gmail.com>
2024-01-08 12:34:25 +0100

    correctif navbar

--- index.js
+++ index.js
@@ -49,3 +49,18 @@ function changeLanguage() {
 document.addEventListener('DOMContentLoaded', function () {
     loadTranslations(); // Charger les traductions au chargement de la page
 });
+
+document.addEventListener('click', function (e) {
+    if (e.target.classList.contains('scroll-link')) {
+        e.preventDefault();
+
+        const targetId = e.target.getAttribute('href').substring(1); // Obtient l'ID de la cible (section)
+        const targetElement = document.getElementById(targetId);
+
+        // Vérifie si la cible existe
+        if (targetElement) {
+            // Utilisez smooth scroll pour une animation agréable
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+});
